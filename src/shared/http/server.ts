276070b5db5d720856d9882de43342ca690680d9
Deivis <deivisutp@gmail.com>
2021-05-04 22:08:45 -0300

    first commit || schedule of the application

---
+++ src/shared/http/server.ts
@@ -0,0 +1,31 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use(routes);
+
+//Error middleware
+app.use((error: Error, _req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('server is running || port 3333');
+});
