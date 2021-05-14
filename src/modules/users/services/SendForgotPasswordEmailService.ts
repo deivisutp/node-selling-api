5ac549a721ba7e7b44f761119b081539c2fe047e
Deivis <deivisutp@gmail.com>
2021-05-14 19:50:24 -0300

    email password

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '../typeorm/repositories/UserRepository';
 import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -17,6 +18,11 @@ class SendForgotPasswordEmailService {
     if (!user) throw new AppError('User email not found.');
 
     const token = await userTokensRepository.generate(user.id);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+    });
   }
 }
 
