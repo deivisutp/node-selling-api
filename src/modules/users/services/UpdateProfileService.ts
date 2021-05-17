82ce329729a6c868a36dad9f46a725584345eade
Deivis <deivisutp@gmail.com>
2021-05-17 20:09:44 -0300

    user profile

---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,54 @@
+import AppError from '@shared/errors/AppError';
+import { compare, hash } from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import { UserRepository } from '../typeorm/repositories/UserRepository';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  password?: string;
+  old_password?: string;
+}
+
+class UpdateProfileService {
+  public async execute({
+    user_id,
+    name,
+    email,
+    password,
+    old_password,
+  }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UserRepository);
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) throw new AppError('User not found');
+
+    const userUpdateEmail = await usersRepository.findByEmail(email);
+
+    if (userUpdateEmail && userUpdateEmail.id !== user.id)
+      throw new AppError('There is another user using this email.');
+
+    if (password && !old_password)
+      throw new AppError('Old password is mandatory to change the password.');
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) throw new AppError('Invalid old password.');
+
+      user.password = await hash(password, 8);
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateProfileService;
